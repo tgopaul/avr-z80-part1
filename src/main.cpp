6ff234966aabd0410e8ae6fd685dfea4f3ba3bbe
Tim Gopaul <tgopaul@gonetworktech.com>
2023-03-18 15:27:45 -0600

    Z80clock frequency set with timer confirmed with scope

--- src/main.cpp
+++ src/main.cpp
@@ -88,7 +88,7 @@ struct Z80Clock {
     RegWGM2::write(2); // select CTC mode
     RegCOM2B::write(1); // toggle OC2B on compare match
     RegOC2B::config_output(); // GPIO must be in output mode
-    RegOCR2A::write(1); // count that resets timer, the clock period - 0 for half crystal speed, 1 shoult give 1/4 crystal speed
+    RegOCR2A::write(1); // count that resets timer, the clock period - 0 for half crystal speed, 1 gives 1/4 crystal speed, onfirmed with oscilloscope 
     RegOCR2B::write(0); // count that toggles OC2B (must be <= OCR2A) - 0 for 8Mhz, 1 for 4Mhz
     RegCS2::write(1); // 0 to stop, 1 for no prescaler, 2 for /8, etc 
   }
